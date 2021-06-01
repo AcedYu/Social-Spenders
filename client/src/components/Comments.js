26932c95105f90dc3ccfaade4abb9e903b4d26de
AcedYu <alexyu299@hotmail.com>
2021-06-01 15:42:46 -0700

    replaced alerts with modals

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,11 +1,16 @@
 import React from "react";
 import API from "../utils/API";
+import { Modal, Button } from "react-bootstrap";
 
 import CommentsEntry from "./CommentsEntry.js"
 
 const Comments = ({ post_id }) => {
   const [comments, setComments] = React.useState([]);
   const [formObject, setFormObject] = React.useState({});
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   React.useEffect(() => {
     getComents(post_id);
@@ -34,39 +39,59 @@ const Comments = ({ post_id }) => {
         setFormObject({});
         getComents(post_id);
       })
-      .catch(err => alert(err + "\nPlease type in valid content for your comment\n REPLACE THIS ALERT WITH A MODAL IF TIME ALLOWS"))
+      .catch(err => handleShow())
   }
 
   return (
-    <div>
-      <div className="input-group mt-1">
-        <input
-          type="text"
-          className="form-control"
-          name="comment"
-          placeholder="Your comment here"
-          onChange={handleInputChange}
-        />
-        <button
-          className="btn btn-primary"
-          type="button"
-          id="button-addon2"
-          onClick={handleSubmit}
-          >
-          Add Comment
-          </button>
-      </div>
-      {
-        comments.map((comment) => (
-          <CommentsEntry
-          user={comment.user.name}
-          pic={comment.user.image}
-          content={comment.content}
-          timestamp={comment.timestamp}
+    <>
+      <div>
+        <div className="input-group mt-1">
+          <input
+            type="text"
+            className="form-control"
+            name="comment"
+            placeholder="Your comment here"
+            onChange={handleInputChange}
           />
-        ))
-      }
-    </div>
+          <button
+            className="btn btn-primary"
+            type="button"
+            id="button-addon2"
+            onClick={handleSubmit}
+          >
+            Add Comment
+          </button>
+        </div>
+        {
+          comments.map((comment) => (
+            <CommentsEntry
+              user={comment.user.name}
+              pic={comment.user.image}
+              content={comment.content}
+              timestamp={comment.timestamp}
+            />
+          ))
+        }
+      </div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Comment Post Failure</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Please type in valid content for your comment
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
 
--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,9 +1,13 @@
 import React from "react";
 import API from "../utils/API";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Modal } from "react-bootstrap";
 
 const CreatePost = () => {
   const [formObject, setFormObject] = React.useState({});
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -19,23 +23,43 @@ const CreatePost = () => {
       .then(res => {
         window.location.reload()
       })
-      .catch(err => alert(err + "\nPlease type in valid content for your post\n REPLACE THIS ALERT WITH A MODAL IF TIME ALLOWS"))
+      .catch(err => handleShow())
   }
 
   return (
-    <Card className="px-3 py-0 my-1">
-      <form onSubmit={handleSubmit}>
-        <h1>Create a New Post</h1>
-        <textarea
-          className="form-control my-1"
-          rows="4"
-          placeholder="Content"
-          name="post"
-          onChange={handleInputChange}
-        />
-        <Button variant="outline-primary" className="mb-1" type="submit">Post</Button>
-      </form>
-    </Card>
+    <>
+      <Card className="px-3 py-0 my-1">
+        <form onSubmit={handleSubmit}>
+          <h1>Create a New Post</h1>
+          <textarea
+            className="form-control my-1"
+            rows="4"
+            placeholder="Content"
+            name="post"
+            onChange={handleInputChange}
+          />
+          <Button variant="outline-primary" className="mb-1" type="submit">Post</Button>
+        </form>
+      </Card>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Post Failure</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Please type in valid content for your post
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
